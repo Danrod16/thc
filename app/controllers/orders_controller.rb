64243a0acaab24c0a9cd0a93d1d1e4a9721cc304
Daniel Rodriguez <daniel.rod93@gmail.com>
2020-09-30 10:58:34 +0200

    FEAT: Today method order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,15 @@ class OrdersController < ApplicationController
   # def create
   #   @order = Order.new()
   # end
+  def weekly
+    @orders = Order.all
+  end
+
+  def today
+    today_date = date.today.strftime("%Y-%m-%d")
+    meal_date = Order.meal_date
+    @orders = Order.where(meal_date: today_date)
+  end
 
   def raw_data
     Order.create_orders
