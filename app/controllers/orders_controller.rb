5fd4522ed224a91f8e20401807705d268ee2045c
Daniel Rodriguez <daniel.rod93@gmail.com>
2020-09-30 15:33:51 +0200

    Feat: front-end monday view v.1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,11 +3,7 @@ class OrdersController < ApplicationController
     @orders = Order.all
   end
 
-  def today
-    today_date = date.today.strftime("%Y-%m-%d")
-    meal_date = Order.meal_date
-    @orders = Order.where(meal_date: today_date)
-  end
+
 
   def index
     @orders = Order.all
