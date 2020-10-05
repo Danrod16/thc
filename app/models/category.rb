14a3df11bedb13ee484d36a7a1e3f3f995c17f70
Daniel Rodriguez <daniel.rod93@gmail.com>
2020-10-05 14:55:51 +0200

    FIX: bug tbc

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   has_many :products, dependent: :destroy
-  has_many :orders, through: :products
+  # has_many :orders, through: :products
 
   def self.fetch_categories(client)
     client.items("5ec79e4a047417e66944efc5").each do |category|
