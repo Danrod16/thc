28b4a2cdfbf9232098320add5932eaa31b853ee7
Daniel Rodriguez <daniel.rod93@gmail.com>
2020-10-13 16:44:04 +0200

    Feat: create delicery groups

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,10 +29,10 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initSortable } from './init_sortable'; // <-- add this
 
-initSortable(); // <-- add this
 
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initSortable(); // <-- add this
 });
