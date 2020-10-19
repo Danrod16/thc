50da086a98f8f6a55a4e92ba345ddeac42183bdc
Daniel Rodriguez <daniel.rod93@gmail.com>
2020-10-19 15:53:50 +0200

    Fix: fixed product varaible

--- app/models/product.rb
+++ app/models/product.rb
@@ -22,8 +22,8 @@ class Product < ApplicationRecord
   end
 
   def self.update_product(product)
-  product = Product.find_by(product_id: product['_id'])
-  product.update(name: product['name'], description: product['description'],
+  new_product = Product.find_by(product_id: product['_id'])
+  new_product.update(name: product['name'], description: product['description'],
                  meal_name: product['meal-name'])
   end
 
