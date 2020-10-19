58965bc385e7ae0e69db8364008027e13b5458e0
Alejandro Bringas <bringasalejandro@gmail.com>
2020-10-19 14:38:19 +0200

    FIX: Refactor stimulus order controller

--- app/javascript/controllers/orders_controller.js
+++ app/javascript/controllers/orders_controller.js
@@ -1,30 +1,88 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = [ 'meals_summary' ];
+  static targets = [ 'mealsRaw', 'snacksRaw', 'dessertsRaw',
+                     'mealsSummary', 'snacksSummary', 'dessertsSummary' ];
 
   connect() {
-    setInterval(this.refresh, 20000);
+    console.log("Meals controller")
+    setInterval(this.refresh, 10000);
   }
 
   refresh = () => {
-  const path = document.querySelector('.table-responsive-summary').dataset.path;
+  const path = document.querySelector('.tab-content').dataset.path;
    fetch(`/${path}`, { headers: { accept: "application/json" } })
    .then(response => response.json())
    .then((data) => {
-     this.meals_summaryTarget.innerHTML = ""
-     data.meals.forEach( e => {
-      this.meals_summaryTarget.insertAdjacentHTML("beforeend",
+
+    // Meals raw table
+    this.mealsRawTarget.innerHTML = ""
+    data.meals.forEach( e => {
+      this.mealsRawTarget.insertAdjacentHTML("beforeend",
         `<tr>
-        <th>${e[0]}</th>
-        <td>${e[1]}</td>
-        <td>${e[2]}</td>
-        <td>${e[3]}</td>
-        <td>${e[4]}</td>
-        <td>${e[5]}</td>
-        <td>${e[6]}</td>
-        </tr>
-        `)});
-   });
+          <td>${e.meal_date}</td>
+          <td>${e.customer_name}</td>
+          <td>Nada</td>
+          <td>${e.meal_size}</td>
+          <td>${e.meal_protein}</td>
+          <td>${e.meal_custom}</td>
+          <td>${e.notes}</td>
+        </tr>`)});
+
+    // Meals summary table
+    this.mealsSummaryTarget.innerHTML = ""
+    data.meals_summary.forEach( e => {
+      this.mealsSummaryTarget.insertAdjacentHTML("beforeend",
+        `<tr>
+          <th>${e[0]}</th>
+          <td>${e[1]}</td>
+          <td>${e[2]}</td>
+          <td>${e[3]}</td>
+          <td>${e[4]}</td>
+          <td>${e[5]}</td>
+          <td>${e[6]}</td>
+        </tr>`)});
+
+    // Snacks raw table
+    this.snacksRawTarget.innerHTML = ""
+    data.snacks.forEach( e => {
+      this.snacksRawTarget.insertAdjacentHTML("beforeend",
+        `<tr>
+          <td>${e.meal_date}</td>
+          <td>${e.customer_name}</td>
+          <td>Nada</td>
+          <td>${e.notes}</td>
+        </tr>`)});
+    
+    // Snacks summary table
+    this.snacksSummaryTarget.innerHTML = ""
+    data.snacks_summary.forEach( e => {
+      this.snacksSummaryTarget.insertAdjacentHTML("beforeend",
+        `<tr>
+          <th>${e[0]}</th>
+          <td>${e[1]}</td>
+        </tr>`)});
+        
+    // Desserts raw table
+    this.dessertsRawTarget.innerHTML = ""
+    data.desserts.forEach( e => {
+    this.dessertsRawTarget.insertAdjacentHTML("beforeend",
+      `<tr>
+        <td>${e.meal_date}</td>
+        <td>${e.customer_name}</td>
+        <td>Nada</td>
+        <td>${e.notes}</td>
+      </tr>`)});
+    
+    // Desserts summary table
+    this.dessertsSummaryTarget.innerHTML = ""
+    data.desserts_summary.forEach( e => {
+      this.dessertsSummaryTarget.insertAdjacentHTML("beforeend",
+        `<tr>
+          <th>${e[0]}</th>
+          <td>${e[1]}</td>
+        </tr>`)});
+
+    });
  }
 }
