c99a76cf0b3f426147cdd141eb2aaf6702c616a4
Daniel Rodriguez <daniel.rod93@gmail.com>
2020-11-04 09:28:09 +0100

    Fix: size pdf

--- app/services/sticker_pdf.rb
+++ app/services/sticker_pdf.rb
@@ -1,6 +1,6 @@
 class StickerPdf
   include Prawn::View
-  
+
   def initialize(selected_orders)
     @selected_orders = selected_orders
     create_stickers
@@ -9,10 +9,10 @@ class StickerPdf
   def create_stickers
     start_new_page(margin: [10, 30, 10, 30])
     define_grid columns: 2, rows: 4, column_gutter: 5
-    
-    grid.show_all
+
+    # grid.show_all
     # stroke_bounds
-    
+
     col = 0
     row = 0
     @selected_orders.each do |order|
@@ -23,7 +23,7 @@ class StickerPdf
           define_grid columns: 2, rows: 4, column_gutter: 5
           col = 0
           row = 0
-        else 
+        else
           if col == 1
             row += 1
             col = 0
@@ -38,7 +38,7 @@ class StickerPdf
 
   def sticker(order)
     bounding_box([6,190], width: 270, height: 190) do
-      stroke_bounds
+      # stroke_bounds
       bounding_box([0,190], width: 270, height: 100) do
         move_down 10
         image "thc-logo-1.png", width: 120
@@ -49,14 +49,14 @@ class StickerPdf
             allergies = order.product.description.split("Allergies:").last.strip
             font_size(7)
             text "(Allergies: #{allergies})"
-            # stroke_bounds 
+            # stroke_bounds
           end
         end
         bounding_box([125, 50], width: 140, height: 140) do
           font_size(8)
           text order.customer_name
           text order.delivery_address
-          text order.telephone 
+          text order.telephone
           text "Size: #{order.meal_size}"
           text "Protein: #{order.meal_protein}"
           text "Customization: #{order.meal_custom}"
@@ -64,12 +64,12 @@ class StickerPdf
           # stroke_bounds
         end
         # stroke_bounds
-      end 
+      end
       bounding_box([0, 108], width: 270, height: 100) do
         image "thc-QRCode.png", width: 85
         draw_text("Scan me for more info", :at => [5, 12], :style => :bold, :size => 6)
-        draw_text("about calories and macros", :at => [5, 6], :style => :bold, :size => 6)     
-        bounding_box([108, 50], width: 170, height: 50) do
+        draw_text("about calories and macros", :at => [5, 6], :style => :bold, :size => 6)
+        bounding_box([108, 45], width: 170, height: 50) do
           font_size(8)
           text "INSTRUCTIONS:", style: :bold
           move_down 5
